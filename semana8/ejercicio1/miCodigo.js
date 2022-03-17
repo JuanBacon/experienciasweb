c266ff3e67e92f9aa6788bcda78775d9214ce463
JuanBacon <juan.bacon.b@outlook.com>
2022-03-16 19:00:55 -0500

    ejercicio en clase miercoles semana 8

--- semana8/ejercicio1/miCodigo.js
+++ semana8/ejercicio1/miCodigo.js
@@ -1,5 +1,11 @@
-console.log("Hola Mundo!");
-let rol; 
-console.log(rol);
-rol = "Student";
-console.log(rol);
+// Ejercicio en clase
+// Encontrar elemento por id
+document.getElementById("saludo").innerHTML="Hola mundo";
+// Encontrar elemento por etiqueta
+document.getElementsByTagName("h1")[0].innerHTML="Soy un titulo";
+// Encontrar elemento por clase
+document.getElementsByClassName("texto")[0].innerHTML="soy un texto";
+// Cambiar una imagen por otra usando js
+document.getElementById("imagensita").src = "https://th.bing.com/th/id/R.b1e375cf2fdd91645d78659e34211b69?rik=1W1J8WXLBIPlHQ&riu=http%3a%2f%2fgreatgamer.ru%2fresize%2fw630%2fimages%2fgames_cover%2fcover_6948.jpg&ehk=BZtHqsv4MzI3YBoFtwOuMH1vM274VwhrxcKy9OyLJy0%3d&risl=&pid=ImgRaw&r=0"
+// Cambiar un texto por otro
+document.getElementById("aburrido").innerHTML = "Esta clase es una chimba"
