e8e6098c7383b33c05823ea500533066aff840fc
JuanBacon <juan.bacon.b@outlook.com>
2022-03-16 19:56:03 -0500

    tarea colores random terminada

--- semana8/ejercicio2/myCode.js
+++ semana8/ejercicio2/myCode.js
@@ -1,16 +1,15 @@
-let valores = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
-
+let values = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+let columns;
 function randomColor(number){
-    let color = "#";
+    let colour = "#";
     for(var i = 0; i < 6; i ++){
-        color += valores[Math.floor(Math.random()*16)]
+        colour += values[Math.floor(Math.random()*16)]
     }
-    document.getElementsByClassName("column")[number].style = `background-color: ${color}`;
-    document.getElementsByTagName("h1")[number].innerHTML = `${color}`;
-
+    document.getElementsByClassName("column")[number].style = `background-color: ${colour}`;
+    document.getElementsByTagName("h1")[number].innerHTML = `${colour}`;
 }
-function firstColor(){
-    for(var i = 0; i <= 3; i++){
+function firstColor(columns){
+    for(var i = 0; i <= columns -1; i++){
         randomColor(i);
     }
 }
