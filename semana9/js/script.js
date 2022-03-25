bbe7a52bdcb31977ea03880d2403889a7c455b77
Juan Diego Bustamante <Juan.Bacon.B@outlook.com>
2022-03-24 21:00:52 -0500

    ejercicio semana9-final

--- semana9/js/script.js
+++ semana9/js/script.js
@@ -1,37 +1,42 @@
-let memesDir = ["meme0","meme1","meme2","meme3","meme4","meme5"];
+let memesDir = ["meme0","meme1","meme2","meme3","meme4","meme5","meme6","meme7","meme8","meme9","meme10","meme11","meme12","meme13","meme14","meme15","meme16","meme17"];
 let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 let randomCaptcha = "";
 let imgDir = "../assets/img/memes/";
 const imgContainer = document.getElementById("memeImg");
 const captcha = document.getElementById("captchaLabel");
 let captchaInput = document.getElementById("captcha");
-
-
-function firstLoad(){
-    imgContainer.src = imgDir + memesDir[0] + ".png"
-    getRandomCaptcha();
-}
-
-function submit(){
-    let captchaInput = document.getElementById("captcha");
-    if(captchaInput.value === randomCaptcha){
-        getRandomImage();
-        getRandomCaptcha();
-        captchaInput.value ="";
-    } else{
-        alert("CAPTCHA INCORRECTO");
-        captchaInput.value ="";
-    }
-}
+let placeError = document.getElementById("textErrorPlace");
+let textError = document.createTextNode("Captcha incorrecto, intentalo de nuevo");
 
 function getRandomCaptcha(){
     randomCaptcha = ""
     for(let i = 0; i < 6; i++){
         randomCaptcha += characters.charAt(Math.floor(Math.random()*36));
     }
-    captcha.innerHTML ="CAPTCHA: " +  randomCaptcha;
+    captcha.innerHTML = randomCaptcha;
 }
 
 function getRandomImage(){
-    imgContainer.src = imgDir + memesDir[Math.floor(Math.random()*5)]+ ".PNG"
+    imgContainer.src = imgDir + memesDir[Math.floor(Math.random()*18)]+ ".PNG"
 }
+
+function firstLoad(){
+    imgContainer.src = imgDir + memesDir[6] + ".png"
+    getRandomCaptcha();
+}
+
+function submit(){
+
+    if(captchaInput.value === randomCaptcha){
+        getRandomImage();
+        getRandomCaptcha();
+        captchaInput.value ="";
+    } else{
+        placeError.appendChild(textError);
+        captchaInput.value ="";
+        setTimeout(function(){
+            textError.parentNode.removeChild(textError);
+        }, 2000);
+    }
+}
+
