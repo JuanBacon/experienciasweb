d30262010413345e705f9bb7b0ce9954b77a9eb6
JuanBacon <juan.bacon.b@outlook.com>
2022-04-06 19:00:31 -0500

    clase localstorage

---
+++ semana11/script.js
@@ -0,0 +1,40 @@
+const button = document.querySelector('button');
+const scoreElement = document.getElementById("scoreElement");
+
+
+if(typeof(localStorage)==='undefined'){
+    console.log("No lo tienes");
+}
+if(localStorage.getItem('score')){
+    scoreElement.innerHTML = localStorage.getItem('score');
+}else {
+    localStorage.setItem('score','0');
+}
+
+button.onclick = () => {
+    const currrenScore = localStorage.getItem('score');
+
+
+    localStorage.setItem('score', parseInt(currrenScore)+1);
+    scoreElement.innerHTML = localStorage.getItem('score')
+}
+
+function guardarLocalstorage(){
+    const persona = {
+        nombre: "Camilo",
+        edad: 23,
+        correo: "nomelose@uao.edu.co"
+    }
+    localStorage.setItem('persona', JSON.stringify(persona));
+}
+
+
+function obtenerLocalStorage(){
+    if(localStorage.getItem('persona')){
+        const persona = JSON.parse(localStorage.getItem('persona'));
+        console.log(persona);
+    }
+}
+
+guardarLocalstorage();
+obtenerLocalStorage();
